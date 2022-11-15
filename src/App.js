eb2c4e688f7cf2a562ed70082efed8ed45778361
Chris Utsey <chrisautsey88@gmail.com>
2022-11-15 04:33:30 -0500

    add prinatability

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import './styles/App.css';
 import PersonalInfo from './components/personalInfo';
 import WorkExperience from './components/workExperience';
 import Education from './components/education'
+import html2pdf from 'html2pdf.js';
+
+function downloadPDF(e) {
+  e.preventDefault();
+  const form = document.querySelector('form');
+  let opt = {
+    margin: 1,
+    filename: 'MyCV.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'cm', format: 'letter', orientation: 'portrait'},
+  }
+  html2pdf(form, opt);
+}
 
 function App() {
   return (
@@ -11,7 +25,7 @@ function App() {
         <WorkExperience />
         <Education />
         <div className="buttonBox">
-          <button className="submitForm">Download PDF</button>
+          <button className="submitForm" onClick={downloadPDF}>Download PDF</button>
         </div>
       </form>
       
